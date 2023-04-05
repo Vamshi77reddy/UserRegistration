d6be4441c49c9c346efe3ef6309ed1c1aa3bc5d3
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-04-05 11:51:13 +0530

    Cleared all samples

--- src/test/java/com/bridgeLabz/UserRegistrationTest.java
+++ src/test/java/com/bridgeLabz/UserRegistrationTest.java
@@ -1,11 +1,18 @@
 package com.bridgeLabz;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class UserRegistrationTest {
-    UserRegistration userRegistration = new UserRegistration();
+    static UserRegistration userRegistration = new UserRegistration();
 
+
+    @BeforeAll
+    public static void init() {
+        System.out.println("Before all");
+        userRegistration = new UserRegistration();
+    }
     @Test
     void givenFirstNameShouldReturnTrue(){
         boolean actualValue = userRegistration.validateFirstName("Vamshi");
@@ -35,5 +42,32 @@ public class UserRegistrationTest {
         boolean actualMobileNumber = userRegistration.validatePassword("vamshI@9");
         Assertions.assertTrue(actualMobileNumber);
         System.out.println("Password Successfully Validate.");
+    }
+    @Test
+    void checkEmailShouldReturnTrue() {
+        String text = "abc@yahoo.com";
+        boolean email = userRegistration.validateEmail(text);
+        Assertions.assertTrue(email);
+        String text1 = "abc@yahoo.com";
+        boolean email1 = userRegistration.validateEmail(text1);
+        Assertions.assertTrue(email1);
+        String text2 = "abc@yahoo.com";
+        boolean email2 = userRegistration.validateEmail(text2);
+        Assertions.assertTrue(email2);
+        String text3 = "abc@yahoo.com";
+        boolean email3 = userRegistration.validateEmail(text3);
+        Assertions.assertTrue(email3);
+        String text4 = "abc@yahoo.com";
+        boolean email4 = userRegistration.validateEmail(text4);
+        Assertions.assertTrue(email4);
+        String text5 = "abc@yahoo.com";
+        boolean email5 = userRegistration.validateEmail(text5);
+        Assertions.assertTrue(email5);
+        String text6 = "abc@yahoo.com";
+        boolean email6 = userRegistration.validateEmail(text6);
+        Assertions.assertTrue(email6);
+
+        System.out.println("All test case successfully Passed!!!!");
+
     }
 }
